655aeb66a4627a8121527d81ce000ff8940a9c21
Paladini,C.E <cepaladini@gmail.com>
2022-09-19 01:48:00 -0300

    Add files via upload

--- workImage/workImage.h
+++ workImage/workImage.h
@@ -1,1 +1,30 @@
+#ifndef WORKIMAGE_H
+#define WORKIMAGE_H
+
+#include <string>
+#include <iostream>
+
+// Classe usada para gerenciar a carga da imagem,
+// e manter suas informações armazenadas para uso
+// posterior, durante o processamento.
+class workImage
+{
+   private:
+      int width, height, nchannels;
+      unsigned char* data;
+            
+   public:
+      void carrega(std::string arquivo);
+      int getW();
+      int getH();
+      int getNC();
+      
+      unsigned char* get_data();
+      
+      workImage();
+      workImage(std::string arquivo);
+      ~workImage();
+};
+
+#endif
 
